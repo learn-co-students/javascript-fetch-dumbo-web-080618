f5cc9723124e46b1d7c1f73355f4d476251ad2d8
Madeline <mcelis13@users.noreply.github.com>
2018-09-20 19:33:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const app = "I don't do much.";
 
-const token = '338f06af182b4502b9bb97cbdb6fe46c6d03e542';
-
 fetch('https://api.github.com/user/repos', {
   headers: {
     Authorization: `token ${token}`
