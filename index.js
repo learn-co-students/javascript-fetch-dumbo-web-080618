a99cbb64f68a0159f6a449981f68846488ed9f5c
vavnilov <vadim.avnilov@gmail.com>
2018-09-20 19:44:16 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 const app = "I don't do much.";
+
+const token = "6b144d4bf1c4608afa7bd0248732dca0cc508f68"
+
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+
+})
+  .then(res => res.json())
+  .then(json => console.log(json))
